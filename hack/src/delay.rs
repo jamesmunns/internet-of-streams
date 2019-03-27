10874a799514c689619e04dabcf7401bd08a5dc4
James Munns <james.munns@ferrous-systems.com>
2019-03-27 12:38:56 +0100

    Include delay

---
+++ hack/src/delay.rs
@@ -0,0 +1,87 @@
+//! Delays
+
+#![allow(dead_code)]
+
+use cast::u32;
+use nrf52832_pac::SYST;
+use cortex_m::peripheral::syst::SystClkSource;
+use embedded_hal::blocking::delay::{DelayMs, DelayUs};
+
+use crate::clocks::HFCLK_FREQ;
+
+/// System timer (SysTick) as a delay provider
+pub struct Delay {
+    syst: SYST,
+}
+
+impl Delay {
+    /// Configures the system timer (SysTick) as a delay provider
+    pub fn new(mut syst: SYST) -> Self {
+        syst.set_clock_source(SystClkSource::Core);
+
+        Delay { syst }
+    }
+
+    /// Releases the system timer (SysTick) resource
+    pub fn free(self) -> SYST {
+        self.syst
+    }
+}
+
+impl DelayMs<u32> for Delay {
+    fn delay_ms(&mut self, ms: u32) {
+        self.delay_us(ms * 1_000);
+    }
+}
+
+impl DelayMs<u16> for Delay {
+    fn delay_ms(&mut self, ms: u16) {
+        self.delay_ms(u32(ms));
+    }
+}
+
+impl DelayMs<u8> for Delay {
+    fn delay_ms(&mut self, ms: u8) {
+        self.delay_ms(u32(ms));
+    }
+}
+
+impl DelayUs<u32> for Delay {
+    fn delay_us(&mut self, us: u32) {
+        // The SysTick Reload Value register supports values between 1 and 0x00FFFFFF.
+        const MAX_RVR: u32 =  0x00FF_FFFF;
+
+        let mut total_rvr = us * (HFCLK_FREQ / 1_000_000);
+
+        while total_rvr != 0 {
+            let current_rvr = if total_rvr <= MAX_RVR {
+                total_rvr
+            } else {
+                MAX_RVR
+            };
+
+            self.syst.set_reload(current_rvr);
+            self.syst.clear_current();
+            self.syst.enable_counter();
+
+            // Update the tracking variable while we are waiting...
+            total_rvr -= current_rvr;
+
+            while !self.syst.has_wrapped() {}
+
+            self.syst.disable_counter();
+        }
+    }
+}
+
+impl DelayUs<u16> for Delay {
+    fn delay_us(&mut self, us: u16) {
+        self.delay_us(u32(us))
+    }
+}
+
+impl DelayUs<u8> for Delay {
+    fn delay_us(&mut self, us: u8) {
+        self.delay_us(u32(us))
+    }
+}

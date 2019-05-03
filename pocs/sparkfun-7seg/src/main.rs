304995d62fcceb689261617c3ed0b3557eb50ab5
James Munns <james.munns@ferrous-systems.com>
2019-05-03 18:46:07 +0200

    Punctuation check

--- pocs/sparkfun-7seg/src/main.rs
+++ pocs/sparkfun-7seg/src/main.rs
@@ -57,7 +57,7 @@ use utils::delay;
 use embedded_timeout_macros::TimeoutError;
 
 mod spark_ser7seg;
-use crate::spark_ser7seg::SevSegSpim;
+use crate::spark_ser7seg::{SevSegSpim, PunctuationFlags as Punc};
 
 
 #[app(device = dwm1001::nrf52832_hal::nrf52832_pac)]
@@ -115,6 +115,63 @@ const APP: () = {
     #[idle(resources = [TIMER, LED_RED_1, LOGGER, RANDOM, DISPLAY])]
     fn idle() -> ! {
         let mut ctr = 0;
+
+        resources.DISPLAY.clear();
+
+        // pub struct PunctuationFlags: u8 {
+        //     const DOT_BETWEEN_1_AND_2        = 0b0000_0001;
+        //     const DOT_BETWEEN_2_AND_3        = 0b0000_0010;
+        //     const DOT_BETWEEN_3_AND_4        = 0b0000_0100;
+        //     const DOT_RIGHT_OF_4             = 0b0000_1000;
+        //     const DOTS_COLON                 = 0b0001_0000;
+        //     const APOSTROPHE_BETWEEN_3_AND_4 = 0b0010_0000;
+        // }
+
+        resources.DISPLAY.write_punctuation(
+            Punc::DOT_BETWEEN_1_AND_2
+        ).unwrap();
+
+        resources.TIMER.start(1_000_000u32);
+        while resources.TIMER.wait().is_err() {}
+
+        resources.DISPLAY.write_punctuation(
+            Punc::DOT_BETWEEN_2_AND_3
+        ).unwrap();
+
+        resources.TIMER.start(1_000_000u32);
+        while resources.TIMER.wait().is_err() {}
+
+        resources.DISPLAY.write_punctuation(
+            Punc::DOT_BETWEEN_3_AND_4
+        ).unwrap();
+
+        resources.TIMER.start(1_000_000u32);
+        while resources.TIMER.wait().is_err() {}
+
+        resources.DISPLAY.write_punctuation(
+            Punc::DOT_RIGHT_OF_4
+        ).unwrap();
+
+        resources.TIMER.start(1_000_000u32);
+        while resources.TIMER.wait().is_err() {}
+
+        resources.DISPLAY.write_punctuation(
+            Punc::DOTS_COLON
+        ).unwrap();
+
+        resources.TIMER.start(1_000_000u32);
+        while resources.TIMER.wait().is_err() {}
+
+        resources.DISPLAY.write_punctuation(
+            Punc::APOSTROPHE_BETWEEN_3_AND_4
+        ).unwrap();
+
+        resources.TIMER.start(1_000_000u32);
+        while resources.TIMER.wait().is_err() {}
+
+        resources.DISPLAY.write_punctuation(
+            Punc::empty()
+        ).unwrap();
 
         loop {
             resources.TIMER.start(8_000u32);
